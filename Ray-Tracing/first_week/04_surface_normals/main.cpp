106e167658fed0a0d8683b5832e87a940a2857d0
thevowels <aungphyohtet1@gmail.com>
2026-07-16 16:53:59 +0700

    surface normals

---
+++ Ray-Tracing/first_week/04_surface_normals/main.cpp
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 15:01:26 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2026/07/16 16:41:05 by aphyo-ht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt_weekend.hpp"
+#include "hittable_list.hpp"
+#include "hittable.hpp"
+#include "sphere.hpp"
+// double hit_sphere(const point3& center, double radius, const ray& r)
+// {
+//     vec3 oc = center - r.origin();
+//     auto a = dot(r.direction(), r.direction());
+//     auto b = -2.0 * dot(r.direction(), oc);
+//     auto c = dot(oc,oc) - radius * radius;
+//     auto discriminant = b*b - 4*a*c;
+    
+//     if (discriminant < 0) {
+//         return -1.0;
+//     } else {
+//         return (-b - std::sqrt(discriminant) ) / (2.0*a);
+//     }
+// }
+
+// double hit_sphere(const point3& center, double radius, const ray& r)
+// {
+//     vec3 oc = center - r.origin();
+//     auto a = r.direction().length_squared();
+//     auto h = dot(r.direction(), oc);
+//     auto c = oc.length_squared() - radius*radius;
+//     auto discriminant = h*h - a*c;
+
+//     if (discriminant < 0) {
+//         return -1.0;
+//     } else {
+//         return (h - std::sqrt(discriminant)) / a;
+//     }
+// }
+
+
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+
+    // Image
+
+    auto aspect_ratio = 16.0/ 9.0;
+    int image_width = 1000;
+
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+    
+    // world
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+
+    // Camera
+    
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/ image_height);
+    auto camera_center = point3(0,0,0);
+
+    // vectors across the horizontal and down the vertical viewport edges
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0,-viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length ) - viewport_u/2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Render
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r, world);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+
+    
+}
